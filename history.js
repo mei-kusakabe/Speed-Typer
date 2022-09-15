069dd4e431f9acf87e697312d9ffe8503e3dd513
Jafrin <jafrin3347@gmail.com>
2022-09-15 13:50:37 +0600

    blog-nav-added

--- history.js
+++ history.js
@@ -5,7 +5,7 @@ function addHistory(questionText, timeTaken, errorCount) {
   newRow.classList.add("card");
 
   newRow.innerHTML = `
-  <h3>${questionText}</h3>
+  h3>${questionText}</h3>
   <div>
   <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
   <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
@@ -25,14 +25,30 @@ function displayHistory() {
   histories.innerHTML = "";
   const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
 
+
+
+
   previousTests.forEach((test) => {
     const newRow = document.createElement("div");
-    newRow.classList.add("card");
+    newRow.classList.add("div");
 
     newRow.innerHTML = `
+
+  <div class ="card" >
+
+  <div class="row row-cols-sm-1 row-cols-lg-3  row-cols-md-3 g-3">
+  
+  <div class ="col-3">
   <h3>${test.questionText}</h3>
   <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+  <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+  </div>
+
+  </div>
+
+  </div>
+ 
+  
   `;
 
     histories.appendChild(newRow);
