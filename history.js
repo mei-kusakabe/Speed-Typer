0c3c386b2758c1bb8eb69446c5c0e008026dc404
Jafrin <jafrin3347@gmail.com>
2022-09-15 14:22:55 +0600

    faq-done

--- history.js
+++ history.js
@@ -30,25 +30,15 @@ function displayHistory() {
 
   previousTests.forEach((test) => {
     const newRow = document.createElement("div");
-    newRow.classList.add("div");
+    newRow.classList.add("card");
 
     newRow.innerHTML = `
 
-  <div class ="card" >
-
-  <div class="row row-cols-sm-1 row-cols-lg-3  row-cols-md-3 g-3">
-  
-  <div class ="col-3">
   <h3>${test.questionText}</h3>
   <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
   <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   </div>
 
-  </div>
-
-  </div>
- 
-  
   `;
 
     histories.appendChild(newRow);
